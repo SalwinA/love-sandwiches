bc5f3d940b0a88f80a94ea3c53a621755985415c
SalwinA <salwino@gmail.com>
2022-06-03 16:19:43 +0000

    Create a function to get the last row of stock worksheet.

--- run.py
+++ run.py
@@ -52,6 +52,20 @@ def update_sales_worksheet(data):
     print('Sales worksheet updated successfully.\n')
     
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """To calculate surplus stock of sandwiches"""
+    print('Calculating surplus data....\n')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+   
+
+def main():
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print('Welcome to Love Sandwiches automation program!!\n')
+main()
